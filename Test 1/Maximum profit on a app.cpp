403302491238e97f60ddae7977708e7f20908392
Deepanshu Dahiya <66161514+Deepanshu-dahiya@users.noreply.github.com>
2022-01-09 23:41:22 +0530

    Maximum profit on a app

---
+++ Test 1/Maximum profit on a app.cpp
@@ -0,0 +1,45 @@
+/*Maximum Profit on App
+Send Feedback
+You have made a smartphone app and want to set its subscription price such that the profit earned is maximised. There are certain users who will subscribe to your app only if their budget is greater than or equal to your price.
+You will be provided with a list of size N having budgets of subscribers and you need to return the maximum profit that you can earn.
+Lets say you decide that price of your app is Rs. x and there are N number of subscribers. So maximum profit you can earn is :
+ m * x
+where m is total number of subscribers whose budget is greater than or equal to x.
+Input format :
+Line 1 : N (No. of subscribers)
+Line 2 : Budget of subscribers (separated by space)
+Output Format :
+ Maximum profit
+Constraints :
+1 <= N <= 10^6
+1 <=budget[i]<=9999
+Sample Input 1 :
+4
+30 20 53 14
+Sample Output 1 :
+60*/
+
+#include<iostream>
+using namespace std;
+
+#include<bits/stdc++.h>
+int maximumProfit(int budget[], int n) {
+    // Write your code here
+sort(budget, budget+n);
+int ans = budget[0];
+for(int i=0; i<n; i++){
+    ans=max(ans,budget[i]*(n-i));
+}
+return ans;
+}
+
+int main() {
+    int n ,*input,i,*cost;
+    cin>>n;
+    input = new int[n];
+    for(i = 0;i < n;i++)
+        cin>>input[i];
+
+    cout << maximumProfit(input, n) << endl;
+
+}
